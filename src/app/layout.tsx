02c3edc2deb42788e68e6dca296b8882bd624999
Piotr Ferens <piotrferens@outlook.com>
2023-07-24 11:55:33 +0200

    Remove logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,21 @@
-import './globals.css'
+import './globals.css';
 
-import { Inter } from 'next/font/google'
-import Image from 'next/image'
-import SantanderLogo from '../../public/santander-logo-horisontal.png'
-import { ReactNode } from 'react'
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 interface RootLayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const RootLayout = ({ children }: RootLayoutProps) => (
   <html lang="en">
     <body className={inter.className}>
-      <header className="py-4 px-6 border-t-3 border-t-red border-b border-b-gray-light">
-        <Image
-          src={SantanderLogo}
-          alt="santander logo"
-          width={290}
-          height={23}
-          priority
-          quality={100}
-          placeholder="blur"
-        />
-      </header>
+      <header className="py-4 px-6 border-t-3 border-t-red border-b border-b-gray-light"></header>
       {children}
     </body>
   </html>
-)
+);
 
-export default RootLayout
+export default RootLayout;
